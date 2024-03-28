7eddc59c563bc42c35b87516ede9b478936eb097
Fengxueguiren <121274570+Alex-hwang@users.noreply.github.com>
2024-03-28 11:32:23 +0800

    Update ArrayUlti.java

--- src/S02/ArrayUlti.java
+++ src/S02/ArrayUlti.java
@@ -2,4 +2,21 @@ package S02;
 
 public class ArrayUlti {
 
+  //私有化构造方法
+	//目的：不让外界创建它的对象
+
+	private ArrayUlti(){}
+
+	//需要定义为静态的，方便使用
+	public static String printArr(int[] arr) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		for(int i = 0; i < arr.length; i++) {
+			if(i == arr.length - 1) {
+				sb.append(arr[i]);
+			}else {
+				sb.append(arr[i]).append(",");
+			}
+		}
+		sb.append("]");
 }
