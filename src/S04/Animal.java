16318834a0fd02b9b24aeba4ec6027792c27fba5
Alex <2253940186@qq.com>
2024-03-28 11:48:26 +0800

    update

---
+++ src/S04/Animal.java
@@ -0,0 +1,11 @@
+package S04;
+
+public class Animal {
+    public void eat() {
+        System.out.println("Animal is eating");
+    }
+
+    public void sleep() {
+        System.out.println("Animal is sleeping");
+    }
+}
---
+++ src/S04/Cat.java
@@ -0,0 +1,7 @@
+package S04;
+
+public class Cat extends Animal {
+    public void catchMouse() {
+        System.out.println("Cat is catching a mouse");
+    }
+}
