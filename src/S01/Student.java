6ed956b50038445e794980549c71773b44162e3a
Alex <2253940186@qq.com>
2024-03-28 11:16:33 +0800

    Initial commit

---
+++ src/S01/Student.java
@@ -0,0 +1,49 @@
+package S01;
+
+public class Student {
+    private String name;
+    private int age;
+    private String gender;
+
+    public Student(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.gender = address;
+    }
+
+    // Getter
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return gender;
+    }
+
+    // Setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setAddress(String address) {
+        this.gender = address;
+    }
+
+    public void study() {
+        System.out.println(name + " is studying");
+    }
+
+    public void show() {
+        System.out.println(name + age + gender);
+    }
+
+
+}
