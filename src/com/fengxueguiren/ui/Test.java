76a2170131509af4f017d2d55ab37700cc7f4744
Alex <2253940186@qq.com>
2024-03-28 18:09:17 +0800

    update

---
+++ src/com/fengxueguiren/ui/Test.java
@@ -0,0 +1,24 @@
+package com.fengxueguiren.ui;
+
+import javax.swing.*;
+
+public class Test {
+    public static void main(String[] args) {
+        //创建一个游戏的主界面
+        JFrame gameJframe = new JFrame();
+        gameJframe.setSize(600, 800);
+        gameJframe.setVisible(true);
+
+        //创建一个登录界面
+        JFrame loginJframe = new JFrame();
+        loginJframe.setSize(300, 400);
+        loginJframe.setVisible(true);
+
+        //创建一个注册界面
+        JFrame registerJframe = new JFrame();
+        registerJframe.setSize(300, 500);
+        registerJframe.setVisible(true);
+
+
+    }
+}
