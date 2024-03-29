1910db78f81cf81475a5879f7ca9e8f09c589c55
Alex <2253940186@qq.com>
2024-03-29 23:28:16 +0800

    update

--- src/com/fengxueguiren/ui/GameJFrame.java
+++ src/com/fengxueguiren/ui/GameJFrame.java
@@ -6,17 +6,15 @@ public class GameJFrame extends JFrame {
     //界面、窗口
     //规定这个是游戏的主界面，和游戏相关的都写在这里面
     public GameJFrame() {
-        //设置窗口的大小
-        this.setSize(600, 800);
-        //设置窗口的标题
-        this.setTitle("风雪归人拼图游戏1.0");
-        //设置界面置顶
-        this.setAlwaysOnTop(true);
-        //设置界面居中
-        this.setLocationRelativeTo(null);
-        //设置窗口的关闭方式
-        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        extracted();
+        JMenuBar jMenuBar = getjMenuBar();
+        //将菜单栏添加到窗口中
+        this.setJMenuBar(jMenuBar);
+        //设置窗口可见
+        this.setVisible(true);
+    }
 
+    private static JMenuBar getjMenuBar() {
         //初始化菜单
         //创建菜单栏
         JMenuBar jMenuBar = new JMenuBar();
@@ -40,15 +38,21 @@ public class GameJFrame extends JFrame {
         //将菜单添加到菜单栏中
         jMenuBar.add(FunctionJMenu);
         jMenuBar.add(AboutJMenu);
+        return jMenuBar;
+    }
 
-        //将菜单栏添加到窗口中
-        this.setJMenuBar(jMenuBar);
-
-
-
-
-
-        //设置窗口可见
-        this.setVisible(true);
+    private void extracted() {
+        //设置窗口的大小
+        this.setSize(600, 800);
+        //设置窗口的标题
+        this.setTitle("风雪归人拼图游戏1.0");
+        //设置界面置顶
+        this.setAlwaysOnTop(true);
+        //设置界面居中
+        this.setLocationRelativeTo(null);
+        //设置窗口的关闭方式
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        //设置窗口的图标
+        this.setIconImage(new ImageIcon("src/com/i/image/icon.jpg").getImage());
     }
 }
