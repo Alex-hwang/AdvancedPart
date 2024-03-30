d6903e5e40cca375096212bfa900792f289cd8b0
Alex <2253940186@qq.com>
2024-03-30 09:32:17 +0800

    update

--- src/com/fengxueguiren/ui/GameJFrame.java
+++ src/com/fengxueguiren/ui/GameJFrame.java
@@ -18,24 +18,6 @@ public class GameJFrame extends JFrame {
         this.setVisible(true);
     }
 
-    private void initImage() {
-        //初始化图片
-        //创建一个面板
-        JPanel jPanel = new JPanel();
-        //创建一个标签
-        JLabel jLabel = new JLabel();
-        //创建一个图片
-        ImageIcon imageIcon = new ImageIcon("src/com/i/image/background.png");
-        //将图片添加到标签中
-        jLabel.setIcon(imageIcon);
-        //将标签添加到面板中
-        jPanel.add(jLabel);
-        //将面板添加到窗口中
-        this.add(jPanel);
-    }
-
-
-
     private static JMenuBar getjMenuBar() {
         //初始化菜单
         //创建菜单栏
@@ -43,12 +25,14 @@ public class GameJFrame extends JFrame {
         //创建菜单
         JMenu FunctionJMenu = new JMenu("功能");
         JMenu AboutJMenu = new JMenu("关于");
+        JMenu OtherJMenu = new JMenu("其他");
         //创建菜单项
         JMenuItem RestartItem = new JMenuItem("重新开始");
         JMenuItem ExitItem = new JMenuItem("退出游戏");
         JMenuItem ReloginItem = new JMenuItem("重新登录");
         JMenuItem HelpItem = new JMenuItem("帮助");
         JMenuItem AboutItem = new JMenuItem("关于我们");
+        JMenuItem OtherItem = new JMenuItem("别点");
 
         //将菜单项添加到菜单中
         FunctionJMenu.add(RestartItem);
@@ -56,16 +40,35 @@ public class GameJFrame extends JFrame {
         FunctionJMenu.add(ExitItem);
         AboutJMenu.add(HelpItem);
         AboutJMenu.add(AboutItem);
+        OtherJMenu.add(OtherItem);
 
         //将菜单添加到菜单栏中
         jMenuBar.add(FunctionJMenu);
         jMenuBar.add(AboutJMenu);
+        jMenuBar.add(OtherJMenu);
         return jMenuBar;
+    }
+
+    private void initImage() {
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                //计算图片的编号
+                int number = i * 4 + j + 1;
+                //创建一个对象
+                //创建一个标签
+                JLabel jLabel = new JLabel(new ImageIcon(STR."D:\\java\\Java\\AdvancedPart\\src\\com\\i\\image\\animal\\animal3\\\{number}.jpg"));
+                //指定图片的位置
+                jLabel.setBounds(105 * j, 105 * i, 105, 105);
+                //把标签添加到窗口中
+                this.getContentPane().add(jLabel);
+            }
+        }
     }
 
     private void extracted() {
         //设置窗口的大小
-        this.setSize(600, 800);
+        this.setSize(420, 480);
         //设置窗口的标题
         this.setTitle("风雪归人拼图游戏1.0");
         //设置界面置顶
@@ -74,6 +77,8 @@ public class GameJFrame extends JFrame {
         this.setLocationRelativeTo(null);
         //设置窗口的关闭方式
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        //取消默认居中方式
+        this.setLayout(null);
         //设置窗口的图标
         this.setIconImage(new ImageIcon("src/com/i/image/icon.jpg").getImage());
     }
--- src/com/fengxueguiren/ui/LoginJFrame.java
+++ src/com/fengxueguiren/ui/LoginJFrame.java
@@ -6,7 +6,7 @@ public class LoginJFrame extends JFrame {
     //界面、窗口
     //规定这个是登录界面，和登录相关的都写在这里面
     public LoginJFrame() {
-        this.setSize(300, 400);
+        this.setSize(488, 500);
         this.setVisible(true);
         //设置窗口的标题
         this.setTitle("拼图游戏 登录");
